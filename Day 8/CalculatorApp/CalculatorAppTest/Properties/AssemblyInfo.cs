87ed311a9d689513d07fd3e67ccfd667e2c6688d
Smith-1809 <smitheshchanda2003@gmail.com>
2026-01-22 11:38:33 +0530

    Day 8 Calculator App - Practice Assignment 1

---
+++ Day 8/CalculatorApp/CalculatorAppTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorAppTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorAppTest")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d8c4d10-ca44-4053-9f33-a1deb22aacc5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day 8/CalculatorApp/CalculatorAppTest/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CalculatorApp;
+
+namespace CalculatorAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Calculator _calculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TestMethod]
+        public void Add_ValidNumbers_ReturnsCorrectSum()
+        {
+            double result = _calculator.Add(10, 5);
+            Assert.AreEqual(15, result, "Addition logic failed.");
+        }
+
+        [TestMethod]
+        public void Add_EdgeCaseZero_ReturnsSameNumber()
+        {
+            double result = _calculator.Add(15, 0);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void Subtract_ValidNumbers_ReturnsCorrectDifference()
+        {
+            double result = _calculator.Subtract(10, 4);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Multiply_ValidNumbers_ReturnsCorrectProduct()
+        {
+            double result = _calculator.Multiply(5, 5);
+            Assert.AreEqual(25, result);
+        }
+
+        [TestMethod]
+        public void Divide_ValidNumbers_ReturnsCorrectQuotient()
+        {
+            double result = _calculator.Divide(10, 2);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Divide_ByZero_ThrowsDivideByZeroException()
+        {
+            _calculator.Divide(10, 0);
+        }
+    }
+}
